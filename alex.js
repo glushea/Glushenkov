945f7446bb7a317806a4355b4c5dc6d7003decb3
Alex Glushenkov <136252068+glushea@users.noreply.github.com>
2023-07-07 13:50:16 +0300

    Add files via upload

--- alex.js
+++ alex.js
@@ -1,21 +1,17 @@
-// function isValidPassword(password, username){
-//     if((password.lenght<8) || (password.indexOf(' ')!==-1) || (password.indexOf(username)!==-1) )return false;
+function isValidPassword(password, username){
+    if((password.lenght<8) || (password.indexOf(' ')!==-1) || (password.indexOf(username)!==-1) )return false;
     
-// return true;
-// }
+return true;
+}
 
 
-// console.log(isValidPassword('aaaaaaaaa1111','aaaaaaaaa'));
+console.log(isValidPassword('aaaaaaaaa1111','aaaaaaaaa'));
 
-// function callAfterTimeout(){
-//     alert("click!")
-// }
+function callAfterTimeout(){
+    alert("click!")
+}
 
-// // setTimeout(callAfterTimeout,2000);
-// const bin = document.querySelector('button');
-// bin.addEventListener('click',callAfterTimeout);
+// setTimeout(callAfterTimeout,2000);
+const bin = document.querySelector('button');
+bin.addEventListener('click',callAfterTimeout);
 
-let nums = [2,3,5,6,7,8];
-
-let multiple = nums.reduce((a,b) => a+b)/nums.length;
-console.log(multiple);
